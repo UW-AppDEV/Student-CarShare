5950ae11b3190f807ad81639e9365a996e0ee8d3
jerry <jerryzhou00@gmail.com>
2014-11-20 11:03:42 -0500

    Side view nav structure, testing http.get

--- www/js/app.js
+++ www/js/app.js
@@ -3,11 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+var app = angular.module('app', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.run(function ($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,13 +13,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
+        // Set the statusbar to use the default style, tweak this to
+        // remove the status bar on iOS or change it to use white instead of dark colors.
       StatusBar.styleDefault();
     }
   });
-})
+});
 
-.config(function($stateProvider, $urlRouterProvider) {
+app.config(function ($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -29,56 +28,46 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
+      .state('page10', {
+          url: '/page10',
+          templateUrl: 'page10.html',
+          controller: 'MainCtrl'
     })
 
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-      url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
-        }
-      }
+      .state('page11', {
+          url: '/page11',
+          templateUrl: 'page11.html'
     })
-
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
-    .state('tab.account', {
-      url: '/account',
-      views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
-        }
-      }
-    });
+  ;
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
 
+    $urlRouterProvider.otherwise('/page10');
 });
 
+app.controller('MainCtrl', function ($scope, $http) {
+    $scope.httpTest = function () {
+        $http.get('http://rest-service.guides.spring.io/greeting', {
+            params: {}
+        })
+            .success(function (data) {
+                alert(JSON.stringify(data, null, 4));
+            })
+            .error(function () {
+            });
+    };
+    $scope.request = function () {
+        $http.get('https://reserve.studentcarshare.ca/webservices/WSUser/WSRest', {
+            params: {}
+        })
+            .success(function (data) {
+                alert("done");
+            })
+            .error(function () {
+            });
+    };
+});
+
+app.controller('SideCtrl', function ($scope, $http) {
+
+});
