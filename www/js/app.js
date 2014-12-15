22924798544ba7ca5ace9a03c43ed9775fbb8af9
jerry <jerryzhou00@gmail.com>
2014-12-15 12:50:06 -0500

    working conncetion to server

--- www/js/app.js
+++ www/js/app.js
@@ -1,169 +1,22 @@
-//=============================Functions==============================
-
-var httpGet = function (URL) {
-    var xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", URL, false);
-    xmlHttp.send(null);
-    return JSON.parse(xmlHttp.responseText);
-}
-
-var lol = function () {
-    alert('lol');
-}
-
-function sha1(msg) {
-    function rotate_left(n, s) {
-        var t4 = ( n << s ) | (n >>> (32 - s));
-        return t4;
-    };
-    function lsb_hex(val) {
-        var str = "";
-        var i;
-        var vh;
-        var vl;
-        for (i = 0; i <= 6; i += 2) {
-            vh = (val >>> (i * 4 + 4)) & 0x0f;
-            vl = (val >>> (i * 4)) & 0x0f;
-            str += vh.toString(16) + vl.toString(16);
-        }
-        return str;
-    };
-    function cvt_hex(val) {
-        var str = "";
-        var i;
-        var v;
-        for (i = 7; i >= 0; i--) {
-            v = (val >>> (i * 4)) & 0x0f;
-            str += v.toString(16);
-        }
-        return str;
-    };
-    function Utf8Encode(string) {
-        string = string.replace(/\r\n/g, "\n");
-        var utftext = "";
-        for (var n = 0; n < string.length; n++) {
-            var c = string.charCodeAt(n);
-            if (c < 128) {
-                utftext += String.fromCharCode(c);
-            }
-            else if ((c > 127) && (c < 2048)) {
-                utftext += String.fromCharCode((c >> 6) | 192);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-            else {
-                utftext += String.fromCharCode((c >> 12) | 224);
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-        }
-        return utftext;
-    };
-    var blockstart;
-    var i, j;
-    var W = new Array(80);
-    var H0 = 0x67452301;
-    var H1 = 0xEFCDAB89;
-    var H2 = 0x98BADCFE;
-    var H3 = 0x10325476;
-    var H4 = 0xC3D2E1F0;
-    var A, B, C, D, E;
-    var temp;
-    msg = Utf8Encode(msg);
-    var msg_len = msg.length;
-    var word_array = new Array();
-    for (i = 0; i < msg_len - 3; i += 4) {
-        j = msg.charCodeAt(i) << 24 | msg.charCodeAt(i + 1) << 16 |
-        msg.charCodeAt(i + 2) << 8 | msg.charCodeAt(i + 3);
-        word_array.push(j);
-    }
-    switch (msg_len % 4) {
-        case 0:
-            i = 0x080000000;
-            break;
-        case 1:
-            i = msg.charCodeAt(msg_len - 1) << 24 | 0x0800000;
-            break;
-        case 2:
-            i = msg.charCodeAt(msg_len - 2) << 24 | msg.charCodeAt(msg_len - 1) << 16 | 0x08000;
-            break;
-        case 3:
-            i = msg.charCodeAt(msg_len - 3) << 24 | msg.charCodeAt(msg_len - 2) << 16 | msg.charCodeAt(msg_len - 1) << 8 | 0x80;
-            break;
-    }
-    word_array.push(i);
-    while ((word_array.length % 16) != 14) word_array.push(0);
-    word_array.push(msg_len >>> 29);
-    word_array.push((msg_len << 3) & 0x0ffffffff);
-    for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
-        for (i = 0; i < 16; i++) W[i] = word_array[blockstart + i];
-        for (i = 16; i <= 79; i++) W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
-        A = H0;
-        B = H1;
-        C = H2;
-        D = H3;
-        E = H4;
-        for (i = 0; i <= 19; i++) {
-            temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-            E = D;
-            D = C;
-            C = rotate_left(B, 30);
-            B = A;
-            A = temp;
-        }
-        for (i = 20; i <= 39; i++) {
-            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-            E = D;
-            D = C;
-            C = rotate_left(B, 30);
-            B = A;
-            A = temp;
-        }
-        for (i = 40; i <= 59; i++) {
-            temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-            E = D;
-            D = C;
-            C = rotate_left(B, 30);
-            B = A;
-            A = temp;
-        }
-        for (i = 60; i <= 79; i++) {
-            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-            E = D;
-            D = C;
-            C = rotate_left(B, 30);
-            B = A;
-            A = temp;
-        }
-        H0 = (H0 + A) & 0x0ffffffff;
-        H1 = (H1 + B) & 0x0ffffffff;
-        H2 = (H2 + C) & 0x0ffffffff;
-        H3 = (H3 + D) & 0x0ffffffff;
-        H4 = (H4 + E) & 0x0ffffffff;
-    }
-    var temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
-    return temp.toLowerCase();
-}
-
 angular.module('ionic.utils', [])
-    .factory('$localstorage', ['$window', function ($window) {
-        return {
-            set: function (key, value) {
-                $window.localStorage[key] = value;
-            },
-            get: function (key, defaultValue) {
-                return $window.localStorage[key] || defaultValue;
-            },
-            setObject: function (key, value) {
-                $window.localStorage[key] = JSON.stringify(value);
-            },
-            getObject: function (key, defaultValue) {
-                return JSON.parse($window.localStorage[key] || JSON.stringify(defaultValue));
-            }
-        }
-    }]);
+.factory('$localstorage', ['$window', function ($window) {
+  return {
+    set: function (key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function (key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function (key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function (key, defaultValue) {
+      return JSON.parse($window.localStorage[key] || JSON.stringify(defaultValue));
+    }
+  }
+}]);
 
-var app = angular.module('app', ['ionic', 'ionic.utils']);
+var app = angular.module('app', ['ionic', 'ionic.utils', 'ngResource']);
 
 app.run(function ($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -176,103 +29,69 @@ app.run(function ($ionicPlatform) {
   });
 });
 
-app.config(function ($stateProvider, $urlRouterProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
   $stateProvider
-      .state('page10', {
-          url: '/page10',
-          templateUrl: 'page10.html',
-          controller: 'MainCtrl'
-    })
-      .state('page11', {
-          url: '/page11',
-          templateUrl: 'page11.html'
-      });
-    $urlRouterProvider.otherwise('/page10');
+  .state('page10', {
+    url: '/page10',
+    templateUrl: 'page10.html',
+    controller: 'MainCtrl'
+  })
+  .state('page11', {
+    url: '/page11',
+    templateUrl: 'page11.html'
+  });
+  $urlRouterProvider.otherwise('/page10');
 });
 
-app.service('VideosService', ['$window', '$rootScope', '$http', '$localstorage', function ($window, $rootScope, $q, $localstorage, $http) {
-    var service = this;
-    this.search = function (term, type, items, page) {
-        results[type].length = 0;
-        data = httpGet('https://www.googleapis.com/youtube/v3/search' +
-        '?key=' + key +
-        '&type=' + type +
-        '&maxResults=' + items +
-        '&part=' + 'id,snippet' +
-        '&fields' + 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle' +
-        '&q=' + term);
-        results[type] = data.items;
-    };
+
+app.factory('web', function($q, $http, $templateCache) {
+  return {
+    get: function(query) {
+      var deferred = $q.defer();
+      var query = '';
+      var url = 'http://168.235.155.40:2000/https://reserve.studentcarshare.ca/webservices/index.php/WSUser/WSRest?' + query;
+      $http.get(url)
+      .success(function(data) {
+        console.log(data);
+        deferred.resolve(data);
+      });
+      return deferred.promise;
+    }
+  };
+});
+
+app.service('$service', ['$window', '$rootScope', '$http', '$localstorage', '$resource', function ($window, $rootScope, $q, $localstorage, $http) {
+  var service = this;
+  this.search = function (term, type, items, page) {
+    results[type].length = 0;
+    data = httpGet('https://www.googleapis.com/youtube/v3/search' +
+                   '?key=' + key +
+                   '&type=' + type +
+                   '&maxResults=' + items +
+                   '&part=' + 'id,snippet' +
+                   '&fields' + 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle' +
+                   '&q=' + term);
+    results[type] = data.items;
+  };
 }]);
 
-
-app.controller('MainCtrl', function ($scope, $http, $localstorage) {
-    $scope.testlocal = $localstorage.get('test', 'local storage is working');
-    $scope.test = function () {
-        alert($scope.testlocal);
-    };
-    $scope.httpTest = function () {
-        data = httpGet('http://rest-service.guides.spring.io/greeting');
-        alert(JSON.stringify(data));
-    };
-    $scope.hashPW = function (pw) {
-        alert(sha1(sha1(pw)));
-        console.log(getJSON(xmlSource));
-    };
-    $scope.request = function () {
-        var url = "https://reserve.studentcarshare.ca/webservices/index.php/WSUser/WSRest?callback=JSON_CALLBACK";
-        $http.jsonp(url).
-            success(function (data) {
-
-            });
-        /*
-         This Code is ALMOST working DO NOT remove jsonp
-         XML is returned correctly across origin, now convert it to JSON and we are done
-         */
-        /*
-         WORKING COPY (INCOMPATIBLE WITH ANGULAR)
-         <html>
-         <head>
-         <!--<script src="deflate.min.js"></script>-->
-         </head>
-         <body>
-         <script>
-         var xmlSource = "https://reserve.studentcarshare.ca/webservices/index.php/WSUser/WSRest?format=xml";
-         var yqlURL = [
-         "http://query.yahooapis.com/v1/public/yql",
-         "?q=" + encodeURIComponent("select * from xml where url='" + xmlSource + "'"),
-         "&format=xml&callback="
-         ].join("");
-         function getJSON(URL) {
-         var xmlHttp = null;
-         xmlHttp = new XMLHttpRequest();
-         xmlHttp.open("GET", URL, false);
-         xmlHttp.send(null);
-         return xmlHttp.responseText;
-         }
-         function myCallback (data){
-         console.log(data);
-         };
-         console.log(getJSON(xmlSource));
-         </script>
-         <script type="text/xml" src="https://reserve.studentcarshare.ca/webservices/index.php/WSUser/WSRest?format=xml&callback=myCallback"></script>
-         <!--ALMOST WORKING OMFG XML<script type="text/javascript" src="https://reserve.studentcarshare.ca/webservices/index.php/WSUser/WSRest?format=xml&callback=myCallback"></script>-->
-         </body>
-         </html>
-         */
-
-        /*
-         data = $http.get('https://reserve.studentcarshare.ca/webservices/index.php/WSUser/WSRest', {
-            params: {}
-        })
-            .success(function (data) {
-         return data;
-            })
-            .error(function () {
-            });
-         alert(JSON.stringify(data));
-         */
-    };
+app.controller('MainCtrl', function ($scope, $http, $localstorage, $service, web) {
+  $scope.testlocal = $localstorage.get('test', 'local storage is working');
+  $scope.test = function () {
+    alert($scope.testlocal);
+  };
+  $scope.httpTest = function () {
+    data = httpGet('http://rest-service.guides.spring.io/greeting');
+    alert(JSON.stringify(data));
+  };
+  $scope.hashPW = function (pw) {
+    alert(sha1(sha1(pw)));
+  };
+  $scope.request = function () {
+    web.get('');
+  };
 });
 
 app.controller('SideCtrl', function ($scope, $http) {
